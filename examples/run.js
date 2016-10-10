3b7ba7da6097219956205b86c1641f479ec3c91d
Brandon Mills <mills.brandont@gmail.com>
2016-10-10 14:38:14 -0400

    Proxy requests to Horizon

--- examples/run.js
+++ examples/run.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const fs = require('fs');
 const http = require('http');
+const httpProxy = require('http-proxy');
 const path = require('path');
 const url = require('url');
 
@@ -21,10 +22,22 @@ try {
 }
 
 const app = express();
+const server = http.createServer(app);
+const proxy = httpProxy.createProxyServer({
+	target: 'http://localhost:8080/',
+	ws: true
+});
 
 app.use(express.static(example));
 
-const server = http.createServer(app);
+app.use((request, response) => {
+	proxy.web(request, response);
+});
+
+server.on('upgrade', (request, socket, head) => {
+	proxy.ws(request, socket, head);
+});
+
 server.listen(process.env.PORT || 8000, process.env.HOSTNAME || 'localhost', () => {
 	const address = server.address();
 	const link = url.format({
