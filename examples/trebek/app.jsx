eb15522937bb0b006d7f1e0afea22d9454a89bf2
Brandon Mills <mills.brandont@gmail.com>
2016-10-11 16:33:36 -0400

    Begin Trebek example

---
+++ examples/trebek/app.jsx
@@ -0,0 +1,131 @@
+'use strict';
+
+const horizon = Horizon({
+	authType: {
+		token: window.authToken,
+		storeLocally: false
+	}
+});
+
+class App extends React.Component {
+
+	constructor() {
+		super();
+
+		// Subscribe to Horizon collections
+		this.answers = horizon('answers');
+		this.questions = horizon('questions');
+		this.scores = horizon('scores');
+
+		// Set initial state
+		this.state = {
+			categories: [],
+			questionsById: {}
+		};
+
+		// Bind event handlers
+		this.handleSelect = this.handleSelect.bind(this);
+	}
+
+	componentDidMount() {
+		this.fetchQuestions();
+	}
+
+	componentWillUnmount() {
+		this.willUnmount = true;
+	}
+
+	async fetchQuestions() {
+		const response = await fetch('/questions.json');
+		const data = await response.json();
+
+		if (!this.willUnmount) {
+			const questionsById = {};
+			const categories = data.map((category) => ({
+				name: category.name,
+				questions: category.questions.map((question) => {
+					questionsById[question.id] = {
+						...question,
+						asked: false
+					};
+					return question.id;
+				})
+			}));
+
+			this.setState({
+				categories,
+				questionsById
+			});
+		}
+	}
+
+	handleSelect(id) {
+		this.setState({
+			questionsById: {
+				...this.state.questionsById,
+				[id]: {
+					...this.state.questionsById[id],
+					asked: true
+				}
+			}
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Board categories={this.state.categories} onSelect={this.handleSelect} questionsById={this.state.questionsById} />
+			</div>
+		);
+	}
+
+}
+
+const Board = ({ categories, onSelect, questionsById }) => (
+	<table className="Board">
+		<Header categories={categories} />
+		<Cells categories={categories} onSelect={onSelect} questionsById={questionsById} />
+	</table>
+);
+
+const Header = ({ categories }) => (
+	<thead>
+		<tr>
+			{categories.map(({ name }) => (
+				<th key={name}>{name}</th>
+			))}
+		</tr>
+	</thead>
+);
+
+const Cells = ({ categories, onSelect, questionsById }) => {
+	// HTML tables are row-based, so transpose the column-based categories
+	const rows = [];
+	const height = Math.max(...categories.map(({ questions }) => questions.length));
+	for (let i = 0; i < height; i++) {
+		rows.push(categories.map(({ questions }) => questions[i]));
+	}
+
+	return (
+		<tbody>
+			{rows.map((row, i) => (
+				<tr key={i}>
+					{row.map((qid) => (
+						<Cell key={qid} onSelect={onSelect} {...questionsById[qid]} />
+					))}
+				</tr>
+			))}
+		</tbody>
+	);
+};
+
+const Cell = ({ asked, id, onSelect, question }) => (
+	<td className="Cell" onClick={() => onSelect(id)}>
+		{asked ? question : '?'}
+	</td>
+);
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
---
+++ examples/trebek/auth.example.js
@@ -0,0 +1,2 @@
+// Copy this file to `auth.js` and ask nicely for a token.
+window.authToken = 'Put your token here.';
