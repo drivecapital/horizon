94b97a8aaa7ae4d782e781bf0a2db2424c461565
Brandon Mills <mills.brandont@gmail.com>
2016-10-12 12:42:51 -0400

    Add jennings starter

---
+++ examples/jennings/app.js
@@ -0,0 +1,19 @@
+// Collection API docs: http://horizon.io/api/collection/
+//
+// Objects in the `clues` collection have the form
+// {
+//   "category": "SNL Skits",
+//   "clue": "This Celebrity Jeopardy character prefers the surname \"Ferguson\".",
+//   "id": "01234567-89ab-cdef-fedc-ba9876543210",
+//   "timestamp": Wed Oct 12 2016 12:30:16 GMT-0400 (EDT)
+// }
+//
+// Objects in the `responses` collection should have the form
+// {
+//   "clueId": "01234567-89ab-cdef-fedc-ba9876543210",
+//   "response": "Who is Burt Reynolds?",
+//   "timestamp": Wed Oct 12 2016 12:31:29 GMT-0400 (EDT),
+//   "userId": "tester"
+// }
+
+console.log('Good luck!');
