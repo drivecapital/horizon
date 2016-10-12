333a23694cab8ad1973bb55ed98a5ca8e98ce1c4
Brandon Mills <mills.brandont@gmail.com>
2016-10-12 17:11:28 -0400

    Rewrite dev server in ES5

--- examples/run.js
+++ examples/run.js
@@ -2,50 +2,54 @@
 
 'use strict';
 
-const express = require('express');
-const fs = require('fs');
-const http = require('http');
-const httpProxy = require('http-proxy');
-const path = require('path');
-const url = require('url');
+var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var httpProxy = require('http-proxy');
+var path = require('path');
+var url = require('url');
 
-const [node, script, example] = process.argv;
-const examplePath = path.join(__dirname, example);
+var example = process.argv[2];
+var examplePath = path.join(__dirname, example);
 try {
-	const stats = fs.statSync(examplePath);
+	var stats = fs.statSync(examplePath);
 	if (!stats.isDirectory()) {
 		throw new Error();
 	}
 } catch (error) {
-	console.error(`${examplePath} must be a directory!`);
+	console.error(examplePath + ' must be a directory!');
 	process.exit(1);
 }
 
-const app = express();
-const server = http.createServer(app);
-const proxy = httpProxy.createProxyServer({
+var app = express();
+var server = http.createServer(app);
+var proxy = httpProxy.createProxyServer({
 	target: process.env.HORIZON || 'http://162.243.22.35/',
 	ws: true
 });
 
 app.use(express.static(example));
 
-app.use((request, response) => {
-	proxy.web(request, response);
+app.use(function (request, response) {
+	return proxy.web(request, response);
 });
 
-server.on('upgrade', (request, socket, head) => {
-	proxy.ws(request, socket, head);
+server.on('upgrade', function (request, socket, head) {
+	return proxy.ws(request, socket, head);
 });
 
-server.listen(process.env.PORT || 8000, process.env.HOSTNAME || 'localhost', () => {
-	const address = server.address();
-	const link = url.format({
-		protocol: 'http',
-		hostname: address.address,
-		port: address.port,
-		pathname: '/'
-	});
-	console.log(`Listening on ${link}.`);
-	console.log('Press Ctrl+C to exit.');
-});
+server.listen(
+	process.env.PORT || 8000,
+	process.env.HOSTNAME || 'localhost',
+	function () {
+		var address = server.address();
+		var link = url.format({
+			protocol: 'http',
+			hostname: address.address,
+			port: address.port,
+			pathname: '/'
+		});
+		console.log('Listening on ' + link);
+		console.log('Press Ctrl+C to exit.');
+	}
+);
